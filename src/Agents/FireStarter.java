536e654ce72e37d8ff13956319afac699b669e8c
catarinamachado <catarinamachado11@gmail.com>
2019-11-04 10:10:28 +0000

    Begin Fire Starter Agent

--- src/Agents/FireStarter.java
+++ src/Agents/FireStarter.java
@@ -1,8 +1,65 @@
 package Agents;
 
 import Logic.World;
+
 import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+
+import java.util.Random;
 
 public class FireStarter extends Agent {
     private World world;
+
+    private static final double startFireProbability = 0.2;
+
+    public void setup(){
+        super.setup();
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setName(getLocalName());
+        sd.setType("FireStarter");
+        dfd.addServices(sd);
+
+        try {
+            DFService.register(this,dfd);
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
+
+        this.addBehaviour(new TickerBehaviour(this, 1000) {
+            @Override
+            protected void onTick() {
+                Random randomGenerator = new Random();
+                boolean startFire = randomGenerator.nextFloat() < startFireProbability;
+
+                if(startFire) {
+                    int randomX = 0;
+                    int randomY = 0;
+                    do {
+                        randomX = randomGenerator.nextInt(World.dimension) + 1;
+                        randomY = randomGenerator.nextInt(World.dimension) + 1;
+                    } while (world.isValid(randomX, randomY));
+
+                    //TODO mete fogo
+                }
+            }
+        });
+    }
+
+    public void takeDown(){
+
+    }
+
+    public World getWorld() {
+        return world;
+    }
+
+    public void setWorld(World world) {
+        this.world = world;
+    }
 }
--- src/Logic/World.java
+++ src/Logic/World.java
@@ -12,4 +12,59 @@ public class World {
     private List<Position> water;
     private List<Position> houses;
     private List<Zone> zones;
+
+    public static final int dimension = 40;
+
+    public boolean isValid(int x, int y){
+        //TODO
+        return false;
+    }
+
+    public List<Fireman> getFireman() {
+        return fireman;
+    }
+
+    public List<Fire> getFire() {
+        return fire;
+    }
+
+    public List<Position> getFuel() {
+        return fuel;
+    }
+
+    public List<Position> getWater() {
+        return water;
+    }
+
+    public List<Position> getHouses() {
+        return houses;
+    }
+
+    public List<Zone> getZones() {
+        return zones;
+    }
+
+    public void setFireman(List<Fireman> fireman) {
+        this.fireman = fireman;
+    }
+
+    public void setFire(List<Fire> fire) {
+        this.fire = fire;
+    }
+
+    public void setFuel(List<Position> fuel) {
+        this.fuel = fuel;
+    }
+
+    public void setWater(List<Position> water) {
+        this.water = water;
+    }
+
+    public void setHouses(List<Position> houses) {
+        this.houses = houses;
+    }
+
+    public void setZones(List<Zone> zones) {
+        this.zones = zones;
+    }
 }
