22bc4ea1e9cb8f0783dac9059a70ccbffd99e779
catarinamachado <catarinamachado11@gmail.com>
2019-11-04 09:37:53 +0000

    Add Classes

---
+++ src/Agents/Drone.java
@@ -0,0 +1,5 @@
+package Agents;
+
+public class Drone extends Fireman {
+
+}
---
+++ src/Agents/FireStarter.java
@@ -0,0 +1,8 @@
+package Agents;
+
+import Logic.World;
+import jade.core.Agent;
+
+public class FireStarter extends Agent {
+    private World world;
+}
