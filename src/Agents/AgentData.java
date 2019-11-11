d69b27ba4fb4e39ec6d7b807db710f8b3c19c89b
catarinamachado <catarinamachado11@gmail.com>
2019-11-11 10:24:26 +0000

    Add getters and setters

--- src/Agents/AgentData.java
+++ src/Agents/AgentData.java
@@ -21,4 +21,99 @@ public class AgentData {
     private int vel;
     private Ocupation ocupation;
 
+    public FiremanType getFiremanType() {
+        return firemanType;
+    }
+
+    public void setFiremanType(FiremanType firemanType) {
+        this.firemanType = firemanType;
+    }
+
+    public Position getStd_position() {
+        return std_position;
+    }
+
+    public void setStd_position(Position std_position) {
+        this.std_position = std_position;
+    }
+
+    public Position getActual_position() {
+        return actual_position;
+    }
+
+    public void setActual_position(Position actual_position) {
+        this.actual_position = actual_position;
+    }
+
+    public Zone getZone() {
+        return zone;
+    }
+
+    public void setZone(Zone zone) {
+        this.zone = zone;
+    }
+
+    public List<Position> getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(List<Position> fuel) {
+        this.fuel = fuel;
+    }
+
+    public List<Position> getWater() {
+        return water;
+    }
+
+    public void setWater(List<Position> water) {
+        this.water = water;
+    }
+
+    public int getCap_max_water() {
+        return cap_max_water;
+    }
+
+    public void setCap_max_water(int cap_max_water) {
+        this.cap_max_water = cap_max_water;
+    }
+
+    public int getCap_max_fuel() {
+        return cap_max_fuel;
+    }
+
+    public void setCap_max_fuel(int cap_max_fuel) {
+        this.cap_max_fuel = cap_max_fuel;
+    }
+
+    public int getCap_water() {
+        return cap_water;
+    }
+
+    public void setCap_water(int cap_water) {
+        this.cap_water = cap_water;
+    }
+
+    public int getCap_fuel() {
+        return cap_fuel;
+    }
+
+    public void setCap_fuel(int cap_fuel) {
+        this.cap_fuel = cap_fuel;
+    }
+
+    public int getVel() {
+        return vel;
+    }
+
+    public void setVel(int vel) {
+        this.vel = vel;
+    }
+
+    public Ocupation getOcupation() {
+        return ocupation;
+    }
+
+    public void setOcupation(Ocupation ocupation) {
+        this.ocupation = ocupation;
+    }
 }
