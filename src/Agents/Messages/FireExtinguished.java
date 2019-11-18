d1e2af9e08ffaffac74070f5534a3cae075ef359
catarinamachado <catarinamachado11@gmail.com>
2019-11-18 10:05:13 +0000

    Update Fire Extinguished

--- src/Agents/Messages/FireExtinguished.java
+++ src/Agents/Messages/FireExtinguished.java
@@ -1,21 +1,8 @@
 package Agents.Messages;
 
-import Util.Position;
-
 import java.io.Serializable;
 
 public class FireExtinguished implements Serializable {
-    private Position firePos;
-
-    public FireExtinguished(Position firePos) {
-        this.firePos = firePos;
-    }
-
-    public Position getPos() {
-        return firePos;
-    }
-
-    public void setPos(Position firePos) {
-        this.firePos = firePos;
+    public FireExtinguished() {
     }
 }
