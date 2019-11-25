8b1da8aaf41cf8d97b47d1bd44ab8a3127873226
Filipe Monteiro <pimonteiro@protonmail.com>
2019-11-25 09:33:48 +0000

    Fix merge conflicts.

--- src/Agents/Behaviours/HandleStationMessages.java
+++ src/Agents/Behaviours/HandleStationMessages.java
@@ -5,23 +5,13 @@ import Agents.Messages.ExtinguishFireData;
 import Agents.Messages.UpdateData;
 import Agents.Station;
 import Logic.Fire;import Agents.Messages.StartedFire;
-import Agents.Station;
-import Logic.Fire;
-import Util.Position;
-import Agents.AgentData;
 import Agents.Messages.FireExtinguished;
-import Agents.Station;
-
-import Logic.Fire;
 import Util.Ocupation;
 import jade.core.AID;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.UnreadableException;
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -31,40 +21,40 @@ public class HandleStationMessages extends CyclicBehaviour {
     public void action() {
         Station s = (Station) myAgent;
         ACLMessage msg = myAgent.receive();
-        if(msg == null){
+        if (msg == null) {
             block();
             return;
         }
-        try{
+        try {
             Object content = msg.getContentObject();
-            switch (msg.getPerformative()){
-                case(ACLMessage.INFORM):
-                    if(content instanceof UpdateData)
+            switch (msg.getPerformative()) {
+                case (ACLMessage.INFORM):
+                    if (content instanceof UpdateData)
                         handleUpdateData(s, msg);
-                    else if(content instanceof FireExtinguished){
+                    else if (content instanceof FireExtinguished) {
                         handleFireExtinguished(s, msg);
-                    }
-                    else if(content instanceof StartedFire) {
+                    } else if (content instanceof StartedFire) {
                         handleFireStarted(s, msg);
                     }
                     break;
-                case(ACLMessage.AGREE):
-                    if(content instanceof ExtinguishFireData)
+                case (ACLMessage.AGREE):
+                    if (content instanceof ExtinguishFireData)
                         handleAcceptRequest(s, msg);
                     break;
-                case(ACLMessage.REFUSE):
-                    if(content instanceof ExtinguishFireData)
+                case (ACLMessage.REFUSE):
+                    if (content instanceof ExtinguishFireData)
                         handleRefuseFireRequest(s, msg);
                     break;
                 default:
                     System.out.println("Wrong message content.");
                     break;
+            }
         } catch (UnreadableException e) {
             e.printStackTrace();
         }
     }
 
-    //TODO verificar quando nao há ninguem que resolva o fogo
+        //TODO verificar quando nao há ninguem que resolva o fogo
     private void handleRefuseFireRequest(Station s, ACLMessage msg) {
         try {
             ExtinguishFireData cont = (ExtinguishFireData) msg.getContentObject();
@@ -118,7 +108,7 @@ public class HandleStationMessages extends CyclicBehaviour {
         }
     }
                 
-    private void handleFireExtinguished(Station s, ACLMessage msg) throws UnreadableException {
+    private void handleFireExtinguished(Station s, ACLMessage msg){
         AID aid = msg.getSender();
 
         Map<AID, Fire> agentAndFire = s.getTreatment_fire().entrySet().stream()
